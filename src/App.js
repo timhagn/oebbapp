a8160bee8c1db28553a5082aea48088b5657b154
Tim Hagn <mail@timhagn.com>
2019-07-22 20:40:48 +0200

    move App to ./src, add InitialScreen

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from 'react'
+import InitialScreen from './InitialScreen'
+
+const App = () => {
+  return (
+    <Fragment>
+      <InitialScreen />
+    </Fragment>
+  )
+}
+
+export default App
--- src/InitialScreen.js
+++ src/InitialScreen.js
@@ -24,7 +24,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen'
 
-const App = () => {
+const InitialScreen = () => {
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
@@ -112,4 +112,4 @@ const styles = StyleSheet.create({
   },
 })
 
-export default App
+export default InitialScreen
